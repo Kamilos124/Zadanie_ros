0ca4a42052bbd0f23ec6067ed6145d66ddedac5b
Kamilos124 <ktroszczynski@wp.pl>
2024-11-21 19:16:43 +0100

    Wstawiony publisher - nie builduje (nie wiem czego kwestia, ale raczej chodzi o sciezki do pliku)

--- include/zadanie_ros/SomePublisher.hpp
+++ include/zadanie_ros/SomePublisher.hpp
@@ -1,28 +1,22 @@
 #ifndef PUBLISH_HPP
 #define PUBLISH_HPP
 
-
 #include <rclcpp/rclcpp.hpp>
 #include "geometry_msgs/msg/pose.hpp"  
+#include "std_msgs/msg/string.hpp"
+using namespace std::chrono_literals;
 
+double randomDoubleInRange(double min, double max, bool exclusive=false);
+void randomizaPose(geometry_msgs::msg::Pose& pose);
 
 class SomePublisher : public rclcpp::Node
 {
     private:
-        void callback_timer() 
-        {
-            auto message = geometry_msgs::msg::Pose;
-            message.data_x = geometry_msgs::msg::Pose::Point[0];
-            message.data_y = geometry_msgs::msg::Pose::Point[1];
-            message.data_z = geometry_msgs::msg::Pose::Point[2];
-            RCLCPP_INFO(this->get_logger(), "Publish point: (%f, %f, %f)", message.data_x, message.data_y, message.data_z);
-            publisher_ -> publish(message);
-        };
+        void callback_position();
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-        size_t count_;
     public:
-        SomePublisher() : Node("some_publisher"), count(0) {};
+        SomePublisher();
 };
 
 #endif
--- src/SomePublisher.cpp
+++ src/SomePublisher.cpp
@@ -1,11 +1,63 @@
 #include <zadanie_ros/SomePublisher.hpp>
 
 
-SomePublisher::SomePublisher() : Node("some_publisher"), count_(0)
+double randomDoubleInRange(double min, double max, bool exclusive)
+{
+    double cos = RAND_MAX;
+    if (exclusive)
+    {
+        return min + (max - min) * ((double)(rand()+1)/(cos+2));
+    }
+    return min + (max - min) * ((double)(rand())/cos);
+}
+
+
+void randomizePose(geometry_msgs::msg::Pose& pose)
+{
+    pose.position.x=randomDoubleInRange(-10,10,false);
+    pose.position.y=randomDoubleInRange(-10,10,false);
+    pose.position.z=randomDoubleInRange(-10,10,false);
+
+    double yaw, pitch, roll;
+
+    yaw = randomDoubleInRange(-M_PI, M_PI,false);
+    pitch = randomDoubleInRange(-M_PI/2, M_PI/2, true);
+    roll = randomDoubleInRange(-M_PI, M_PI,false);
+
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+
+    pose.orientation.x = sr * cp * cy - cr * sp * sy;
+    pose.orientation.y = cr * sp * cy + sr * cp * sy;
+    pose.orientation.z = cr * cp * sy - sr * sp * cy;
+    pose.orientation.w = cr * cp * cy + sr * sp * sy;
+}
+
+
+SomePublisher::SomePublisher() : Node("some_publisher")
 {
     publisher_ = this->create_publisher<std_msgs::msg::String>("/myPose", 20);
-    timer_ = this->create_wall_timer(500ms, std::bind(&SomePublisher::callback_timer, this)); 
+    timer_ = this->create_wall_timer(500ms, std::bind(&SomePublisher::callback_position, this)); 
+    
 }
+
+
+void SomePublisher::callback_position() 
+{
+    auto wiadomosc = std_msgs::msg::String();
+    auto pose = geometry_msgs::msg::Pose();
+    randomizePose(pose);
+    std::string message_position = std::to_string(pose.position.x) + ", " + std::to_string(pose.position.y) + ", " + std::to_string(pose.position.z) + ", ";
+    std::string message_orientation = std::to_string(pose.orientation.w) + ", " + std::to_string(pose.orientation.x) + ", " + std::to_string(pose.orientation.y) + ", " + std::to_string(pose.orientation.z);
+    std::string message = "Publish point: " + message_position + message_orientation; 
+    RCLCPP_INFO(this->get_logger(), message.c_str());
+    wiadomosc.data = "Pozycja i Orientacja"; 
+    publisher_ -> publish(wiadomosc);
+};
 
 int main(int argc, char **argv)
 {
